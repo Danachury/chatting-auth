f042e02598876e1f8573c050b9884e9b619cc4ac
Denilson Anachury <danachury@easysol.net>
2020-06-26 15:02:12 -0500

    Update SqlStatement.java

--- data-connect/src/main/java/com/dac/chatting/entities/SqlStatement.java
+++ data-connect/src/main/java/com/dac/chatting/entities/SqlStatement.java
@@ -9,7 +9,15 @@ public final class SqlStatement {
     public static final String QUERY_ALL = "SELECT * FROM accounts";
 
     @Contract(pure = true)
-    public static @NotNull(exception = NullArgumentException.class) String WHERE(final String statement) {
-        return QUERY_ALL + " WHERE " + statement;
+    public static @NotNull(exception = NullArgumentException.class)
+    String WHERE(String... params) {
+        return QUERY_ALL + " WHERE " + buildWhere(params);
+    }
+
+    @Contract(pure = true)
+    public static @NotNull(exception = NullArgumentException.class)
+    String buildWhere(String... statement) {
+        // ToDo: Replace this builder by other implementation
+        return String.join(" = ?", statement);
     }
 }
